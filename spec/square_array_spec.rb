7599299632a3f87d226279914c37d43ae5d07a05
Ting Ting Zheng <ttzhenng@gmail.com>
2020-09-29 05:53:53 +0000

    Automatically backed up by Learn

--- spec/square_array_spec.rb
+++ spec/square_array_spec.rb
@@ -1,20 +1,16 @@
-describe '#square_array' do
-
-  numbers.each do
+def square_array do
+  
     numbers = [1,2,3]
-    expect(numbers).to_not receive(:collect)
-    expect(numbers).to_not receive(:map)
-    expect(numbers).to_not receive(:inject)
     square_array(numbers)
   end
-
-  it 'calls on each' do
+  
+  numbers.each do
     numbers = [1,2,3]
     expect(numbers).to receive(:each)
     square_array(numbers)
   end
 
-  it 'should square the elements in an array' do
+  numbers.collect do
     expect(square_array([1,2,3])).to eq([1,4,9])
     expect(square_array([9,10,16,25])).to eq([81,100,256,625])
   end
