2ab871c3647700ab9cf214ed2d6bf9ade3c25d05
Ting Ting Zheng <ttzhenng@gmail.com>
2020-09-29 05:56:26 +0000

    Automatically backed up by Learn

--- spec/square_array_spec.rb
+++ spec/square_array_spec.rb
@@ -1,7 +1,8 @@
 def square_array
-    numbers each do
-    numbers = [1,2,3]
-    square_array(numbers)
+  
+  numbers.each do
+  numbers = [1,2,3]
+  square_array(numbers)
   end
   
   numbers.each do
