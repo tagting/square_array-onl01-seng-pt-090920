64d6e0ad14872ac86ba769c7fc10a5e2503e54cc
Ting Ting Zheng <ttzhenng@gmail.com>
2020-09-29 05:56:49 +0000

    Automatically backed up by Learn

--- spec/square_array_spec.rb
+++ spec/square_array_spec.rb
@@ -5,13 +5,13 @@ def square_array
   square_array(numbers)
   end
   
-  numbers.each do
+  numbers.collecy do
     numbers = [1,2,3]
     expect(numbers).to receive(:each)
     square_array(numbers)
   end
 
-  numbers.collect do
+  numbers.inject do
     expect(square_array([1,2,3])).to eq([1,4,9])
     expect(square_array([9,10,16,25])).to eq([81,100,256,625])
   end
